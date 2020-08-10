1f74ebdfeb46e0fbd211d6c50d1f2bc7de14b681
shun-shobon <shun.shobon@gmail.com>
2020-08-10 19:06:03 +0900

    build: add webpack setting file

---
+++ webpack.config.ts
@@ -0,0 +1,41 @@
+import * as path from "path";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import { ConfigurationFactory } from "webpack";
+
+const config: ConfigurationFactory = () => ({
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: {
+    "content/index": path.join(__dirname, "src", "content", "index.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          { loader: "babel-loader" },
+          { loader: "ts-loader" },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, "static"),
+          to: ".",
+        },
+      ],
+    }),
+  ],
+});
+
+export default config;
